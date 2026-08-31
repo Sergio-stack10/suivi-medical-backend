9e2b649ebe1621a71e12d29bbd9d17a6cc0f0f60
Sergio-stack10 <botobelexis@gmail.com>
2026-08-31 14:49:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ import traceback
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 jours = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
